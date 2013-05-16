0f59b176b86a851b69d387d18f4ee3b3be256b30
Nikita Lvov <niklvov@gmail.com>
2013-05-16 23:26:36 +0400

    create public folder + move all from there

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,41 +5,35 @@ module.exports = function(grunt) {
         less: {
             development: {
                 options: {
-                    paths: ["source/static/less"]
+                    paths: ["source/client/static/less"]
                 },
                 files: {
-                    "source/static/css/app.css": "source/static/less/app.less",
-                    "source/static/css/admin.css": "source/static/less/admin.less"
+                    "source/client/static/css/app.css": "source/client/static/less/app.less",
+                    "source/client/static/css/admin.css": "source/client/static/less/admin.less"
                 }
             }
         },
         cssmin: {
             compress: {
                 files: {
-                    "source/static/css/app.min.css": [
-                        "source/static/css/bootstrap.min.css", "source/static/css/typicons.css",
-                        "source/static/css/app.css"
+                    "source/public/css/app.min.css": [
+                        "source/client/static/css/bootstrap.min.css", "source/client/static/css/typicons.css",
+                        "source/client/static/css/app.css"
                     ],
-                    "source/static/css/admin.min.css": [
-                        "source/static/css/bootstrap.min.css", "source/static/css/bootstrap-responsive.min.css",
-                        "source/static/css/bootstrap-colorpicker.css", "source/static/css/admin.css"
+                    "source/public/css/admin.min.css": [
+                        "source/client/static/css/bootstrap.min.css", "source/client/static/css/bootstrap-responsive.min.css",
+                        "source/client/static/css/bootstrap-colorpicker.css", "source/client/static/css/admin.css"
                     ]
                 }
-            }
-        },
-        dotjs: {
-            options: {},
-            files: {
-                "source/static/js/template.js": ['source/views/partials/gamePage.dot']
             }
         },
         coffee : {
             compile: {
                 files : {
-                    'source/static/js/app.body.js':["source/client/models/*.coffee", "source/client/collections/*.coffee",
+                    'source/public/js/app.body.js':["source/client/models/*.coffee", "source/client/collections/*.coffee",
                         "source/client/views/**/**.coffee", "source/client/app.coffee"
                     ],
-                    'source/static/js/admin.body.js':["source/client/admin.coffee"]
+                    'source/public/js/admin.body.js':["source/client/admin.coffee"]
                 },
                 options: {
                     bare: true
@@ -49,20 +43,14 @@ module.exports = function(grunt) {
         uglify : {
             head: {
                 files : {
-                    "source/static/js/app.head.js" :[
-                        "source/static/js/jquery-1.9.1.min.js", "source/static/js/jquery.cookie.js",
-                        "source/static/js/underscore.js", "source/static/js/backbone.js",
-                        "source/static/js/infiniScroll.js", "source/static/js/dot.min.js",
-                        "source/static/js/swfobject.js"
+                    "source/public/js/app.head.js" :[
+                        "source/client/static/js/jquery-1.9.1.min.js", "source/client/static/js/jquery.cookie.js",
+                        "source/client/static/js/underscore.js", "source/client/static/js/backbone.js",
+                        "source/client/static/js/infiniScroll.js", "source/client/static/js/dot.min.js",
+                        "source/client/static/js/swfobject.js"
                     ]
                 }
             }
-            /*,
-            body: {
-                files: {
-                    "source/static/js/app.body.js": ["source/static/js/coffee.js"]
-                }
-            } */
         }
     });
 
@@ -71,7 +59,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-dotjs');
 
     grunt.registerTask('default', ['less', 'cssmin', 'uglify', 'coffee']);
     //grunt.registerTask('default', 'dotjs');
